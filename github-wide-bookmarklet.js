a3e279e7092fb71fa8dd443a8acf6876c95e7c1a
Susanna Kyler <35513177+susMoose@users.noreply.github.com>
2021-03-19 11:50:58 -0600

    Update github-wide-bookmarklet.js

--- github-wide-bookmarklet.js
+++ github-wide-bookmarklet.js
@@ -1,10 +1,10 @@
 // Sourced from https://sethflowers.com/2017/06/27/make-github-code-review-full-width.html
 // Bookmark must look like this 
 // javascript:var makeFullWidth = function(element) { element.style.width = document.body.clientWidth + 'px'; }; var content = document.getElementsByClassName('repository-content')[0]; makeFullWidth(content); makeFullWidth(content.closest('.container'));
+javascript:
 var makeFullWidth = function(element) {
-  element.style.width = document.body.clientWidth + 'px';
+  var wide = document.body.clientWidth - (document.body.clientWidth / 4 );
+  element.style.maxWidth = wide  + 'px';
 };
-
-var content = document.getElementsByClassName('repository-content')[0];
+var content = document.getElementsByClassName('container-lg clearfix')[1];
 makeFullWidth(content);
-makeFullWidth(content.closest('.container'));
